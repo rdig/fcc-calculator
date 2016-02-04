36d94adbfae0f156c265450882e6503bae6ebe0d
Raul Glogovetan <raul@glogovetan.com>
2016-02-04 10:51:04 +0200

    refactored the code into a proper Calculator() class

--- sources/scripts/boilerplate.js
+++ sources/scripts/boilerplate.js
@@ -1,152 +1,214 @@
-(function () {
+const Calculator = class {
 
-	let keys = $('.key');
-	let screen = $('#screen');
-	let overflow = $('.current');
-	let backspace = $('.backspace');
-	let operators = $('.operator');
-	// let history = $('#history');
+	constructor(configObject) {
 
-	let update = function(newInput) {
-
-		switch (newInput) {
-			// Function: Clear all
-			case 'c':
-				updateShadowMemmory._clearAll();
-				break;
-			// Function: Backspace
-			case 'back':
-				updateShadowMemmory.backspace();
-				break;
-			// Operator: Addition
-			case 'add':
-				console.log('add')
-				break;
-			// Operator: Subtraction
-			case 'sub':
-				console.log('subtract')
-				break;
-			// Operator: Multiplication
-			case 'mul':
-				console.log('multiply')
-				break;
-			// Operator: Division
-			case 'div':
-				console.log('divide')
-				break;
-			// Operator: Percent
-			case 'per':
-				console.log('percent of')
-				break;
-			// Operator: Equal
-			case 'equ':
-				console.log('equals')
-				break;
-			// Special: Decimal point
-			case 'dec':
-				updateShadowMemmory.decimal();
-				break;
-			default:
-				updateShadowMemmory.key(newInput);
-				break;
-		}
-	};
-
-	let updateScreen = function(val, display = screen) {
-		display.html(val);
-	};
-
-	// let updateHistory = function(val) {};
-
-	let updateInterface = {
-		key: function(keyElement) {
-			return {
-				_isOn: function() {
-					return keyElement.hasClass('active');
-				},
-				on: function() {
-					keyElement.addClass('active');
-				},
-				off: function() {
-					keyElement.removeClass('active');
-				}
-			}
-		},
-		screenOverflow: {
-			on: function() {
-				overflow.addClass('overflow');
-			},
-			off: function() {
-				overflow.removeClass('overflow');
-			}
-		}
-	};
-
-	let updateShadowMemmory = {
-		memmory : {
+		let mem = {
 			chain: [],
 			current: '0',
-			decCount: false
-		},
-		_addCurrentMem: function(val) {
-			if (this.memmory.current !== '0') {
-				this.memmory.current += val;
-			} else {
-				this.memmory.current = val;
-				updateInterface.key(backspace).on();
-				updateInterface.key(operators).on();
+			decimal: false
+		};
+
+		this.config = {};
+
+		// Handle the memmory i/o
+		this.mem = {
+			get current() {
+				return mem.current;
+			},
+			set current(value) {
+				mem.current = value;
+			},
+			get decimal() {
+				return mem.decimal;
+			},
+			set decimal(bool) {
+				mem.decimal = bool;
+			},
+			get chain() {
+				return mem.chain;
+			},
+			set chain(array) {
+				mem.chain = array;
 			}
-			if (this.memmory.current.length > 10) {
-				updateInterface.screenOverflow.on();
-			} else {
-				updateInterface.screenOverflow.off();
-			}
-			updateScreen(this.memmory.current);
-		},
-		_removeLastCurrent: function() {
-			if (this.memmory.current !== '0') {
-				this.memmory.current = this.memmory.current.slice(0, -1);
-				if (this.memmory.current === '') {
-					this.memmory.current = '0';
-					updateInterface.key(backspace).off();
-					updateInterface.key(operators).off();
-					updateInterface.screenOverflow.off();
-				}
-				if (this.memmory.current.length < 11) {
-					updateInterface.screenOverflow.off();
-				}
-			}
-			updateScreen(this.memmory.current);
-		},
-		_clearAll: function() {
-			this.memmory = {
-				chain: [],
-				current: '0',
-				decCount: false
-			};
-			updateInterface.key(backspace).off();
-			updateInterface.key(operators).off();
-			updateInterface.screenOverflow.off();
-			updateScreen(this.memmory.current);
-		},
-		backspace: function() {
-			if (updateInterface.key(backspace)._isOn()) {
-				this._removeLastCurrent();
-			}
-		},
-		key: function(val) {
-			this._addCurrentMem(val);
-		},
-		decimal: function() {
-			if (!this.memmory.decCount) {
-				this._addCurrentMem('.');
-				this.memmory.decCount = true;
+		};
+
+		// Select the elements that were passed via CONFIG
+		for (let prop in configObject) {
+			if ((configObject[prop]) && (configObject[prop] !== '')) {
+				this.config[prop] = $(configObject[prop]);
 			}
 		}
-	};
 
-	keys.on('click', function() {
-		update(this.dataset.value);
+		// Set the click event handler for the elements
+		this.config.keys.on('click', { Calculator: this }, function(e) {
+			e.data.Calculator.update(this.dataset.value).mouseInteraction();
+		});
+
+	}
+
+	update(input) {
+
+		// Perfom actions on the Shadow Memmory
+		const shadowMemory = function() {
+
+			const _addCurrentMem = function(val, decimal = false) {
+				if (this.mem.current !== '0') {
+					if (decimal) {
+						if (!this.mem.decimal) {
+							this.mem.current += val;
+							this.mem.decimal = true;
+						}
+					} else {
+						this.mem.current += val;
+						dashboard.key();
+					}
+				} else {
+					if (!decimal) {
+						this.mem.current = val;
+					}
+					dashboard.key(this.config.backspace).on();
+					dashboard.key(this.config.operators).on();
+				}
+				// If the number doesn't fit on the screen add overflow visual que
+				if (this.mem.current.length > 10) {
+					dashboard.screen(this.config.overflow).on();
+				} else {
+					dashboard.screen(this.config.overflow).off();
+				}
+				display(this.mem.current);
+			}.bind(this);
+
+			const _removeLastCurrent = function() {
+				if (this.mem.current !== '0') {
+					this.mem.current = this.mem.current.slice(0, -1);
+					if (this.mem.current === '') {
+						this.mem.current = '0';
+						dashboard.key(this.config.backspace).off();
+						dashboard.key(this.config.operators).off();
+					}
+					if (this.mem.current.length < 11) {
+						dashboard.screen(this.config.overflow).off();
+					}
+					display(this.mem.current);
+				}
+			}.bind(this);
+
+			const _clearAll = function() {
+				this.mem.chain = [];
+				this.mem.current = '0';
+				this.mem.decimal = false;
+				dashboard.key(this.config.backspace).off();
+				dashboard.key(this.config.operators).off();
+				dashboard.screen(this.config.overflow).off();
+				display(this.mem.current);
+			}.bind(this);
+
+			return {
+				clear: function() {
+					_clearAll();
+				},
+				backspace: function() {
+					_removeLastCurrent();
+				},
+				key: function() {
+					_addCurrentMem(input);
+				},
+				decimal: function() {
+					_addCurrentMem('.', true);
+				}
+			}
+
+		}.bind(this);
+
+		// Perfom actions on the calculator display
+		const display = function(value) {
+			this.config.screen.html(value);
+		}.bind(this);
+
+		// Visuals of the calculator interface (functions / operators / screen)
+		const dashboard = {
+			key: function(element) {
+				return {
+					on: function() {
+						element.addClass('active');
+					},
+					off: function() {
+						element.removeClass('active');
+					}
+				}
+			},
+			screen: function(element) {
+				return {
+					on: function() {
+						element.addClass('overflow');
+					},
+					off: function() {
+						element.removeClass('overflow');
+					}
+				}
+			}
+		};
+
+		return {
+			mouseInteraction: function() {
+				switch (input) {
+					// Function: Clear all
+					case 'c':
+						shadowMemory().clear();
+						break;
+					// Function: Backspace
+					case 'back':
+						shadowMemory().backspace();
+						break;
+					// Operator: Addition
+					case 'add':
+						console.log('add')
+						break;
+					// Operator: Subtraction
+					case 'sub':
+						console.log('subtract')
+						break;
+					// Operator: Multiplication
+					case 'mul':
+						console.log('multiply')
+						break;
+					// Operator: Division
+					case 'div':
+						console.log('divide')
+						break;
+					// Operator: Percent
+					case 'per':
+						console.log('percent of')
+						break;
+					// Operator: Equal
+					case 'equ':
+						console.log('equals')
+						break;
+					// Special: Decimal point
+					case 'dec':
+						shadowMemory().decimal();
+						break;
+					default:
+						shadowMemory().key();
+						break;
+				}
+			},
+			keyboardInteraction: function() {}
+		}
+	}
+};
+
+(function () {
+
+	new Calculator({
+
+		// element hooks, will be passed to jquery's selector
+		keys: '.key',
+		screen: '#screen',
+		overflow: '.current',
+		backspace: '.backspace',
+		operators: '.operator'
+		
 	});
 
 }());
